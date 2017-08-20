2111375a97b68a3946d43080ddf09a6c958d6069
chenxili <1421025233@qq.com>
2017-08-20 23:06:20 +0800

    init code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// 搭建入口文件
+import React from 'react'
+import {render} from 'react-dom'
+import {Provider} from 'react-redux'
+import {browerHistory} from 'react-router'
+import {syncHistoryWithStore} from 'react-router-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 引入项目的相关配置
+import configureStore from './store/configureStore'
+import getRouter from './router'
+
+// 引入公共的css样式
+import './common/public.scss'
+
+import fastClick from 'fastclick'
+fastClick.attach(document.body)
+
+const store = configureStore()
+const history = syncHistoryWithStore(browerHistory, store)
+const router = getRouter()
+
+render(
+    <Provider store={store}>
+        {router}
+    </Provider>,
+    document.getElementById('root')
+)
+
 registerServiceWorker();
